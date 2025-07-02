4f7ec6b4715d06f971987c989c19ee5391f8c1dd
MRARE66C <tvsupegame@gmail.com>
2025-07-02 17:09:21 +0700

    Refactor App component to implement a counter and step functionality; remove unused test file.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // const initDate = new Date();
+
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  // const [date, setDate] = useState(initDate.toDateString());
+
+  // useEffect(() => {
+  //   const newDate = new Date();
+  //   newDate.setDate(newDate.getDate() + count);
+  //   setDate(newDate.toDateString());
+  // }, [count]);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>
+          <button onClick={() => (step > 1 ? setStep((s) => s - 1) : "")}>
+            -
+          </button>
+          Step: {step}
+          <button onClick={() => setStep((s) => s + 1)}>+</button>
+        </h2>
+      </div>
+      <div className="container">
+        <h2>
+          <button onClick={() => setCount((s) => s - step)}>-</button>Count:{" "}
+          {count} <button onClick={() => setCount((s) => s + step)}>+</button>
+        </h2>
+      </div>
+      <h1>
+        {count >= 1
+          ? `${count} days from today is `
+          : count === 0
+          ? "Today is "
+          : `${Math.abs(count)} days ago is `}
+        {date.toDateString()}
+      </h1>
     </div>
   );
 }
