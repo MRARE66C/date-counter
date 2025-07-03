b8901929b4ed22fb1daf7ab9ea0422df6805ac5b
MRARE66C <tvsupegame@gmail.com>
2025-07-03 13:24:43 +0700

    Enhance App component with slider for step input and number input for count; add reset button and update styling for better layout.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function App() {
-  // const initDate = new Date();
-
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
-  // const [date, setDate] = useState(initDate.toDateString());
-
-  // useEffect(() => {
-  //   const newDate = new Date();
-  //   newDate.setDate(newDate.getDate() + count);
-  //   setDate(newDate.toDateString());
-  // }, [count]);
-
   const date = new Date();
   date.setDate(date.getDate() + count);
 
   return (
     <div className="App">
       <div className="container">
-        <h2>
-          <button onClick={() => (step > 1 ? setStep((s) => s - 1) : "")}>
-            -
-          </button>
-          Step: {step}
-          <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <h2 className="slider">
+          Step:
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
+          <p>{step}</p>{" "}
         </h2>
       </div>
       <div className="container">
         <h2>
-          <button onClick={() => setCount((s) => s - step)}>-</button>Count:{" "}
-          {count} <button onClick={() => setCount((s) => s + step)}>+</button>
+          <button onClick={() => setCount((s) => s - step)}>-</button>
+          <input
+            type="number"
+            placeholder="Enter day"
+            value={!count ? "" : count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          ></input>
+          <button onClick={() => setCount((s) => s + step)}>+</button>
         </h2>
       </div>
       <h1>
         {count >= 1
           ? `${count} days from today is `
-          : count === 0
+          : count === 0 || !count
           ? "Today is "
           : `${Math.abs(count)} days ago is `}
         {date.toDateString()}
       </h1>
+      <button
+        onClick={() => {
+          setCount(0);
+          setStep(0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
